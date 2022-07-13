eb35bca447852b0af848b0a3939f9ba4fbd2ed48
TXT-66 <txt-45@TXT-66.lan>
2022-07-13 15:21:19 -0700

    geolocation

--- tabs/Hours.js
+++ tabs/Hours.js
@@ -13,14 +13,55 @@ import {
   Linking,
   TouchableOpacity,
 } from 'react-native'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import React, { useState, useEffect } from 'react'
+import * as Location from 'expo-location'
 
 import styles from './styles/styles'
 
 export default function Hours() {
- return(
-<View>
- <MapView style={styles.map} />
-</View>
-)}
+  const [location, setLocation] = useState(null)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied')
+        return
+      }
+
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+      console.log(location)
+    })()
+  }, [])
+
+  let text = 'Waiting..'
+  if (errorMsg) {
+    text = errorMsg
+  } else if (location) {
+    text = JSON.stringify(location)
+  }
+  return (
+    <View>
+      {location === null ? null : (
+        <MapView
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+          style={styles.map}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            // image={require('../assets/marker.png')}
+          />
+        </MapView>
+      )}
+    </View>
+  )
+}
